8bf88bc3c3940c387bd79fcd96fc3760348b714f
Luke Anderson <lukeqanderson@gmail.com>
2022-01-04 20:11:59 +0900

    added delete functionality to each individual task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Go to dog park",
+      day: "Feb 2nd at 2:15pm",
+      reminder: true,
+    }
+  ]);
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
 
   return (
     <div className="container">
       <Header/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} 
+      onDelete={deleteTask}/> : 'No Tasks'}
     </div>
   );
 }
